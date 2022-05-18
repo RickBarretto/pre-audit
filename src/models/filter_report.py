ff38e87cb341f5892a38adab88437c23d0fab124
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-18 14:04:30 -0300

    New filter, that filter reports, not all data

---
+++ src/models/filter_report.py
@@ -0,0 +1,25 @@
+"""Filters results from OSV API
+"""
+
+from models.utils.flatten_list import flatten_list
+
+
+class FilterByReport:
+    """Filters results from OSV API"""
+
+    @staticmethod
+    def get_report_id(report: dict) -> str:
+        return report["id"]
+
+    @staticmethod
+    def get_report_description(report: dict) -> str:
+        return report["details"]
+
+    @staticmethod
+    def get_report_aliases(report: dict) -> list:
+        return report["aliases"]
+
+    @staticmethod
+    def get_report_affected_versions(report: dict) -> list:
+        versions_arrays = [sub["versions"] for sub in report["affected"]]
+        return flatten_list(versions_arrays)
---
+++ tests/unit/test_models/test_filter_report.py
@@ -0,0 +1,48 @@
+from pytest import fixture
+
+from data.jinja_fetched_data import RAW_DATA
+
+from models.filter_report import FilterByReport
+
+
+data = RAW_DATA
+reports = data.get("vulns")
+entry = reports[0]
+
+
+def test_get_id():
+    assert FilterByReport.get_report_id(entry) == "PYSEC-2014-8"
+
+
+def test_get_description():
+    description = "The default configuration for bccache.FileSystemBytecodeCache in Jinja2 before 2.7.2 does not properly create temporary files, which allows local users to gain privileges via a crafted .cache file with a name starting with __jinja2_ in /tmp."
+    assert FilterByReport.get_report_description(entry) == description
+
+
+def test_get_aliases():
+    assert FilterByReport.get_report_aliases(entry) == ["CVE-2014-1402"]
+
+
+def test_get_affected_versions():
+    versions = [
+        "2.0",
+        "2.0rc1",
+        "2.1",
+        "2.1.1",
+        "2.2",
+        "2.2.1",
+        "2.3",
+        "2.3.1",
+        "2.4",
+        "2.4.1",
+        "2.5",
+        "2.5.1",
+        "2.5.2",
+        "2.5.3",
+        "2.5.4",
+        "2.5.5",
+        "2.6",
+        "2.7",
+        "2.7.1",
+    ]
+    assert FilterByReport.get_report_affected_versions(entry) == versions
