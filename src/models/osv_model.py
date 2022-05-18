da3b70fc8f16b7db3ad326ba3d607e88c72fd1aa
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-17 22:42:58 -0300

    The Request Model data

---
+++ src/models/osv_model.py
@@ -0,0 +1,21 @@
+"""Defines The API Model data"""
+
+
+class OsvModel:
+    def __init__(self, package: str, version: str):
+        self.package = package
+        self.version = version
+
+    def get_data(self) -> str:
+        model = str(
+            {"version": version, "package": {"name": package, "ecosystem": "PyPI"}}
+        )
+        return model
+
+
+class OsvUrl:
+    def __init__(self):
+        self.url = "https://api.osv.dev/v1/query"
+
+    def get_url(self):
+        return self.url
