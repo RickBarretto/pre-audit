456f81faf115d0df58822fe7c8b42e7597116d7d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-20 18:30:43 -0300

    remove `staticmethod` decorators
    - the methods'll pick the instance's values

--- src/core/filter_report.py
+++ src/core/filter_report.py
@@ -26,23 +26,18 @@ class FilterByReport:
     def get_id(report: dict) -> str:
         return report["id"]
 
-    @staticmethod
     def get_summary(report: dict) -> str:
         return report["summary"]
 
-    @staticmethod
     def get_description(report: dict) -> str:
         return report["details"]
 
-    @staticmethod
     def get_aliases(report: dict) -> list:
         return report["aliases"]
 
-    @staticmethod
     def get_affected_versions(report: dict) -> list:
         versions_arrays = [sub["versions"] for sub in report["affected"]]
         return flatten_list(versions_arrays)
 
-    @staticmethod
     def get_references(report: dict) -> list:
         return report["references"]
