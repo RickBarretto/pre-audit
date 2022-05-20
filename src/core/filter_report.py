6ff4514a3e9f70266ce33e6f2b4ca0b59dd23cc8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-20 19:58:18 -0300

    fix self missin and avoid overengineering

--- src/core/filter_report.py
+++ src/core/filter_report.py
@@ -1,10 +1,9 @@
 """Filters results from OSV API
 """
 
-from typing import List
+from typing import List, Dict
 
 from src.core.utils.flatten_list import flatten_list
-from src.core.utils.generics.generic_models import Index
 
 
 class FilterByReport:
@@ -15,44 +14,44 @@ class FilterByReport:
     `vulns` is a list with reports, and each item is one report.
 
     Methods:
-        - get_main_info(self) -> list[ tuple[ReportId, ReportDescription] ]
-        - get_all_affected_versions(self) -> ReportVersions
-        - get_id(self, report: Index) -> ReportId
-        - get_summary(self, report: Index) -> ReportSummary
-        - get_description(self, report: Index) -> ReportDescription
-        - get_aliases(self, report: Index) -> ReportAliases
-        - get_affected_versions(self, report: Index) -> ReportVersions
-        - get_references(self, report: Index) -> ReportReferences
+        - get_main_info(self) -> list[ tuple[str, str] ]
+        - get_all_affected_versions(self) -> list[str]
+        - get_id(self, report_index: int) -> str
+        - get_summary(self, report_index: int) -> str
+        - get_description(self, report_index: int) -> str
+        - get_aliases(self, report_index: int) -> list[str]
+        - get_affected_versions(self, report_index: int) -> list[str]
+        - get_references(self, report_index: int) -> list[dict[str, str]]
     """
 
     def __init__(self, fetched_data: dict):
         self.reports: list = fetched_data.get("vulns")
 
-    def get_main_info(self) -> List[tuple[ReportId, ReportDescription]]:
+    def get_main_info(self) -> List[tuple[str, str]]:
         """Get the ID and Detail from all reports"""
-        return [(report["id"], report["details"]) for report in reports]
+        return [(report["id"], report["details"]) for report in self.reports]
 
-    def get_all_affected_versions(self) -> ReportVersions:
+    def get_all_affected_versions(self) -> List[str]:
         """Get all affected versions from all reports"""
         versions_arrays = [sub["affected"]["versions"] for sub in self.reports]
         return flatten_list(versions_arrays)
 
-    def get_id(self, report: Index) -> ReportId:
+    def get_id(self, report_index: int) -> str:
         """Get a specific report's id"""
-        return report["id"]
+        return self.reports[report_index]["id"]
 
-    def get_summary(self, report: Index) -> ReportSummary:
+    def get_summary(self, report_index: int) -> str:
         """Get a specific report's summary"""
-        return report["summary"]
+        return self.reports[report_index]["summary"]
 
-    def get_description(self, report: Index) -> ReportDescription:
+    def get_description(self, report_index: int) -> str:
         """Get a specific report's description"""
-        return report["details"]
+        return self.reports[report_index]["details"]
 
-    def get_aliases(self, report: Index) -> ReportAliases:
+    def get_aliases(self, report_index: int) -> List[str]:
         """Get a specific report's aliases"""
-        return report["aliases"]
+        return self.reports[report_index]["aliases"]
 
-    def get_references(self, report: Index) -> ReportReferences:
+    def get_references(self, report_index: int) -> List[Dict[str, str]]:
         """Get a specific report's references"""
-        return report["references"]
+        return self.reports[report_index]["references"]
