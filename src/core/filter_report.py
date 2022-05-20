aa95716a562542031c3bf95e134f8f802dbee38d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-20 19:07:15 -0300

    update typing

--- src/core/filter_report.py
+++ src/core/filter_report.py
@@ -1,6 +1,8 @@
 """Filters results from OSV API
 """
 
+from typing import List
+
 from src.core.utils.flatten_list import flatten_list
 from src.core.utils.generics.index_model import Index
 
@@ -13,36 +15,44 @@ class FilterByReport:
     `vulns` is a list with reports, and each item is one report.
 
     Methods:
-        - def get_id(report: dict) -> str:
-        - def get_summary(report: dict) -> str:
-        - def get_description(report: dict) -> str:
-        - def get_aliases(report: dict) -> list:
-        - def get_affected_versions(report: dict) -> list:
-        - def get_references(report: dict) -> list:
+        - get_main_info(self) -> list[ tuple[ReportId, ReportDescription] ]
+        - get_all_affected_versions(self) -> ReportVersions
+        - get_id(self, report: Index) -> ReportId
+        - get_summary(self, report: Index) -> ReportSummary
+        - get_description(self, report: Index) -> ReportDescription
+        - get_aliases(self, report: Index) -> ReportAliases
+        - get_affected_versions(self, report: Index) -> ReportVersions
+        - get_references(self, report: Index) -> ReportReferences
     """
 
     def __init__(self, fetched_data: dict):
         self.reports: list = fetched_data.get("vulns")
 
-    def get_main_info(self) -> List[tuple]:
-        """Get the ID and Detail from each report"""
+    def get_main_info(self) -> List[tuple[ReportId, ReportDescription]]:
+        """Get the ID and Detail from all reports"""
         return [(report["id"], report["details"]) for report in reports]
 
-    def get_all_affected_versions(self) -> list:
+    def get_all_affected_versions(self) -> ReportVersions:
+        """Get all affected versions from all reports"""
         versions_arrays = [sub["affected"]["versions"] for sub in self.reports]
         return flatten_list(versions_arrays)
 
-    def get_id(self, report: Index) -> str:
+    def get_id(self, report: Index) -> ReportId:
+        """Get a specific report's id"""
         return report["id"]
 
-    def get_summary(self, report: Index) -> str:
+    def get_summary(self, report: Index) -> ReportSummary:
+        """Get a specific report's summary"""
         return report["summary"]
 
-    def get_description(self, report: Index) -> str:
+    def get_description(self, report: Index) -> ReportDescription:
+        """Get a specific report's description"""
         return report["details"]
 
-    def get_aliases(self, report: Index) -> list:
+    def get_aliases(self, report: Index) -> ReportAliases:
+        """Get a specific report's aliases"""
         return report["aliases"]
 
-    def get_references(self, report: Index) -> list:
+    def get_references(self, report: Index) -> ReportReferences:
+        """Get a specific report's references"""
         return report["references"]
