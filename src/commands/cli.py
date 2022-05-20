ce7b8ce40b94b7626595fe314d22286d313daaee
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-20 20:46:05 -0300

    add action to audit command

--- src/commands/cli.py
+++ src/commands/cli.py
@@ -1,9 +1,18 @@
 import click
 
+from src.core.fetch_api import fetch_api
+from src.core.filter_report import Filter
+from src.core.utils.osv_model import OsvModel
+
 
 @click.command(name="package")
 @click.argument("package")
 @click.argument("version")
 def audit_package(package, version):
-    reports = fetch(package, version)
-    Standard(reports)
+    data = fetch_api(OsvModel(package, version))
+    f = Filter(data)
+    click.secho("Vulnerabilities founded!\n", fg="red", bold=True)
+    info = f.get_main_info()
+    for i in info:
+        click.secho("{}: ".format(i[0]), fg="red", bold=True)
+        click.secho("{}\n".format(i[1]), fg="blue")
