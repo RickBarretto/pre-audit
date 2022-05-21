b868d694648a75b0e07c53e1463db8f5b70f04af
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-21 14:35:04 -0300

    fix test version to work on right way

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -1,5 +1,7 @@
+import pkg_resources as pkg_info
+
 from version import VERSION
 
 
 def test_version():
-    assert VERSION == "0.0.0"
+    assert VERSION == pkg_info.get_distribution("pre-audit").version
