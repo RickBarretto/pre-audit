bccf491463531333b283c1f7fd5fa4bc9f4ac1c9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-05-21 12:47:55 -0300

    Issue #1:
    - turn API function into class
    - raise error
    - requests are did by internal method

--- src/core/fetch_api.py
+++ src/core/fetch_api.py
@@ -7,8 +7,32 @@ from src.core.utils.osv_model import OsvModel, OsvUrl
 from src.core.utils.exceptions import PackageNotFound
 
 
-def fetch_api(osv_model: OsvModel) -> dict:
-    """Fetch the OSV API and return a Json"""
+import requests
+
+
+class OsvApi:
+    """Deal with OsvApi to get package Vulnerabilities
+
+    usage:
+    >>> django_pkg = OsvModel("Django", "3.0")
+    >>> response = OsvApi(django_pkg).fetch()
+    {...}
+    """
+
+    def __init__(self, osv_parameters: OsvModel):
+
+        # Attributes
+        self.api_parameters = osv_parameters.get_data()
+
+    def fetch(self):
+        json: dict = self.__request()
+        if json:
+            return json
+        else:
+            raise PackageNotFound
+
+    def __request(self) -> dict:
+        """Fetch the OSV API and return a Json"""
 
         osv_link = "https://api.osv.dev/v1/query"
 
