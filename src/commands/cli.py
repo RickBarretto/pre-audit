67e048a134a8fee93bcb6467127433e22b771034
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-11 15:56:10 -0300

    use `message_type` api

--- src/commands/cli.py
+++ src/commands/cli.py
@@ -3,6 +3,8 @@
 import click
 from requests.exceptions import HTTPError, Timeout, ConnectionError
 
+from src.commands.ux import message_type
+
 from src.core.fetch_api import OsvApi
 from src.core.filter_report import Filter
 from src.core.utils.exceptions import PackageNotFound
@@ -18,17 +20,18 @@ def audit_package(package, version):
         data = OsvApi(package, version).fetch()
 
         f = Filter(data)
-        click.secho("Vulnerabilities founded!\n", fg="red", bold=True)
+        message_type.warn("Vulnerabilities founded!\n")
         info = f.get_main_info()
 
         for i in info:
-            click.secho("{}: ".format(i[0]), fg="red", bold=True)
-            click.secho("{}\n".format(i[1]), fg="blue")
+            message_type.warn("{}: ".format(i[0]))
+            message_type.info("{}\n".format(i[1]))
+
     except PackageNotFound:
-        click.secho("Package isn't in OSV's DataBase!\n", fg="red", bold=True)
+        message_type.warn("Package isn't in OSV's DataBase!\n")
     except HTTPError as err:
-        click.secho("Http Error: {}\n".format(err), fg="red", bold=True)
+        message_type.warn("Http Error: {}\n".format(err))
     except ConnectionError as err:
-        click.secho("Connection Error!\n".format(err), fg="red", bold=True)
+        message_type.warn("Connection Error!\n".format(err))
     except Timeout:
-        click.secho("Request Timeout! :(\n", fg="red", bold=True)
+        message_type.warn("Request Timeout! :(\n")
