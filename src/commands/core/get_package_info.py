f3d614b7074aed3627f9764c3d9fde0fa3262b65
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-11 21:31:43 -0300

    change api

--- src/commands/core/get_package_info.py
+++ src/commands/core/get_package_info.py
@@ -1,7 +1,19 @@
+"""Deals with core functions"""
+
 from src.core.fetch_api import OsvApi
 from src.core.filter_report import Filter
 
 
-def get_package_info(package: str, version: str) -> list:
-    data = OsvApi(package, version).fetch()
-    return Filter(data).get_main_info()
+def fetch(package: str, version: str) -> list:
+    """Fetches the data from OsvApi"""
+    return Filter(OsvApi(package, version).fetch())
+
+
+def get_main_info(filtred_data: Filter) -> list:
+    """Get the main info to print"""
+    return filtred_data.get_main_info()
+
+
+def get_all_affected_versions_info(filtred_data: Filter) -> list:
+    """Gets all affected version"""
+    return filtred_data.get_all_affected_versions()
