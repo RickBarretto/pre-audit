a375e61eae069f115abf3a179e0ffb0e953507b9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-11 21:39:32 -0300

    hide main information when --affected is True

--- src/commands/core/audit_command.py
+++ src/commands/core/audit_command.py
@@ -8,10 +8,11 @@ def run(package: str, version: str, has_all_affected_option: bool):
     """Empower the audit command"""
 
     data = get_package_info.fetch(package, version)
-    info = get_package_info.get_main_info(data)
-
-    audit_command_ui.echo_main(info)
 
     if has_all_affected_option:
         affected = get_package_info.get_all_affected_versions_info(data)
         audit_command_ui.echo_affected(affected)
+
+    else:
+        info = get_package_info.get_main_info(data)
+        audit_command_ui.echo_main(info)
