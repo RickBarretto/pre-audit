3e4fc528ce28f7d09a67528ac20942e74b818384
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-17 16:54:36 -0300

    test `--affected` option

--- tests/unit/commands/test_audit_output.py
+++ tests/unit/commands/test_audit_output.py
@@ -1,11 +1,24 @@
 from click.testing import CliRunner
+from pytest_print import printer
+
 from src.commands.cli import audit_package
 
 
-def test_package_audit():
+def test_package_audit_command():
     runner = CliRunner()
     result = runner.invoke(audit_package, ["Django", "3.2"])
+
     assert result.exit_code == 0
     assert not result.exception
     assert "Vulnerabilities founded!" in result.output
     assert "GHSA-" in result.output
+
+
+def test_all_affected_versions_command(printer):
+    runner = CliRunner()
+    result = runner.invoke(audit_package, ["Django", "3.2", "--affected"])
+
+    assert result.exit_code == 0
+    assert not result.exception
+    assert "Affected versions" in result.output
+    assert "." in result.output
