88fec3ab589ea82296ec911b8b9f5078d9eac4d3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-17 23:40:05 -0300

    format code

--- tests/unit/test_core/test_flatten_list.py
+++ tests/unit/test_core/test_flatten_list.py
@@ -4,4 +4,5 @@ from core.utils.flatten_list import flatten_list
 def test_function():
     entry = [[0, 1, 2], [3, 4, 5, 6], [7, 8, 9, 10]]
     expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     assert flatten_list(entry) == expected
