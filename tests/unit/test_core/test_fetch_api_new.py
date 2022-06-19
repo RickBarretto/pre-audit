fb1c5e0f22a89c395a19d0e41600ba0c1d6735df
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-18 23:06:44 -0300

    refactor methods inside classes

--- tests/unit/test_core/test_fetch_api_new.py
+++ tests/unit/test_core/test_fetch_api_new.py
@@ -1,7 +1,6 @@
 from unittest import mock
 
 import pytest
-from pytest_print import printer
 
 import responses
 import requests
@@ -10,76 +9,78 @@ from src.core.fetch_api import OsvApi
 from src.core.utils.exceptions import PackageNotFound
 
 
-@responses.activate
-def test_api_ok():
+class TestApi:
+    @responses.activate
+    def test_api_ok(self):
 
-    responses.add(
-        method="POST",
-        url="https://api.osv.dev/v1/query",
-        json={"vulns": [{"id": "GHSA-xxxx-xxxx-xxxx"}, {"id": "GHSA-yyyy-yyyy-yyyy"}]},
-        status=200,
-    )
+        responses.add(
+            method="POST",
+            url="https://api.osv.dev/v1/query",
+            json={
+                "vulns": [{"id": "GHSA-xxxx-xxxx-xxxx"}, {"id": "GHSA-yyyy-yyyy-yyyy"}]
+            },
+            status=200,
+        )
 
-    req = requests.post("https://api.osv.dev/v1/query")
+        req = requests.post("https://api.osv.dev/v1/query")
 
-    assert req.json() == {
-        "vulns": [{"id": "GHSA-xxxx-xxxx-xxxx"}, {"id": "GHSA-yyyy-yyyy-yyyy"}]
-    }
-    assert req.status_code == 200
+        assert req.json() == {
+            "vulns": [{"id": "GHSA-xxxx-xxxx-xxxx"}, {"id": "GHSA-yyyy-yyyy-yyyy"}]
+        }
+        assert req.status_code == 200
 
+    @responses.activate
+    def test_requests_with_wrong_package(self):
 
-@responses.activate
-def test_requests_with_wrong_package():
+        responses.add(
+            method="POST",
+            url="https://api.osv.dev/v1/query",
+            json={},
+            status=200,
+        )
 
-    responses.add(
-        method="POST",
-        url="https://api.osv.dev/v1/query",
-        json={},
-        status=200,
-    )
-
-    req = requests.post("https://api.osv.dev/v1/query")
-    assert req.json() == {}
-    assert req.status_code == 200
-
-
-@mock.patch("src.core.fetch_api.requesting.fetch")
-def test_package_not_found(mocked_fetch, printer):
-
-    mocked_fetch.return_value = {}
-
-    with pytest.raises(PackageNotFound) as err:
-        OsvApi("FakePkg", "3.2").fetch()
-    assert True
-    printer("Exception tested: {0}".format(err))
+        req = requests.post("https://api.osv.dev/v1/query")
+        assert req.json() == {}
+        assert req.status_code == 200
 
 
-@responses.activate
-def test_http_errors(printer):
+class TestExceptions:
+    @mock.patch("src.core.fetch_api.requesting.fetch")
+    def test_package_not_found(self, mocked_fetch, printer):
 
-    error404 = responses.Response(
-        method="POST",
-        url="https://api.osv.dev/v1/query",
-        status=404,
-    )
+        mocked_fetch.return_value = {}
 
-    error512 = responses.Response(
-        method="POST",
-        url="https://api.osv.dev/v1/query",
-        status=512,
-    )
+        with pytest.raises(PackageNotFound) as err:
+            OsvApi("FakePkg", "3.2").fetch()
+        assert True
+        printer("Exception tested: {0}".format(err))
 
-    responses.add(error404)
-    responses.add(error512)
+    @responses.activate
+    def test_http_errors(self, printer):
 
-    with pytest.raises(requests.HTTPError) as err:
-        req_4xx = requests.post("https://api.osv.dev/v1/query")
-        req_4xx.raise_for_status()
-    assert req_4xx.status_code == 404
-    printer("Exception tested: {0}".format(err))
+        error404 = responses.Response(
+            method="POST",
+            url="https://api.osv.dev/v1/query",
+            status=404,
+        )
 
-    with pytest.raises(requests.HTTPError) as err:
-        req_5xx = requests.post("https://api.osv.dev/v1/query")
-        req_5xx.raise_for_status()
-    assert req_5xx.status_code == 512
-    printer("Exception tested: {0}".format(err))
+        error512 = responses.Response(
+            method="POST",
+            url="https://api.osv.dev/v1/query",
+            status=512,
+        )
+
+        responses.add(error404)
+        responses.add(error512)
+
+        with pytest.raises(requests.HTTPError) as err:
+            req_4xx = requests.post("https://api.osv.dev/v1/query")
+            req_4xx.raise_for_status()
+        assert req_4xx.status_code == 404
+        printer("Exception tested: {0}".format(err))
+
+        with pytest.raises(requests.HTTPError) as err:
+            req_5xx = requests.post("https://api.osv.dev/v1/query")
+            req_5xx.raise_for_status()
+        assert req_5xx.status_code == 512
+        printer("Exception tested: {0}".format(err))
