a44efea8c0a54b65979eddfeff215683f8c621b3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-19 13:36:34 -0300

    move to audit command

--- src/commands/core/audit_command.py
+++ src/commands/core/audit_command.py
@@ -1,18 +1,38 @@
 """Low level of audit command"""
 
-from src.commands.core import get_package_info
 from src.commands.ux import audit_command_ui
+from src.core.fetch_api import OsvApi
+from src.core.filter_report import Filter
+
+__all__ = ["run"]
+
+
+class _Utils:
+    @staticmethod
+    def fetch(package: str, version: str) -> list:
+        """Fetches the data from OsvApi"""
+        return Filter(OsvApi(package, version).fetch())
+
+    @staticmethod
+    def get_main_info(filtred_data: Filter) -> list:
+        """Get the main info to print"""
+        return filtred_data.get_main_info()
+
+    @staticmethod
+    def get_all_affected_versions_info(filtred_data: Filter) -> list:
+        """Gets all affected version"""
+        return filtred_data.get_all_affected_versions()
 
 
 def run(package: str, version: str, has_all_affected_option: bool):
     """Empower the audit command"""
 
-    data = get_package_info.fetch(package, version)
+    data = _Utils.fetch(package, version)
 
     if has_all_affected_option:
-        affected = get_package_info.get_all_affected_versions_info(data)
+        affected = _Utils.get_all_affected_versions_info(data)
         audit_command_ui.echo_affected(affected)
 
     else:
-        info = get_package_info.get_main_info(data)
+        info = _Utils.get_main_info(data)
         audit_command_ui.echo_main(info)
