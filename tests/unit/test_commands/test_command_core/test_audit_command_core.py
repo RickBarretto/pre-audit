1f3ebbc0ada2229e7a0c144f281a9789d68664bc
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-06-19 15:27:24 -0300

    create test

---
+++ tests/unit/test_commands/test_command_core/test_audit_command_core.py
@@ -0,0 +1,20 @@
+import pytest
+from unittest import mock
+from click.testing import CliRunner
+
+from src.commands.core.audit_core import _Utils, run, Filter
+
+
+class TestUtils:
+    @mock.patch("src.commands.core.audit_core.OsvApi.fetch")
+    def test_fetch(self, mock_osv):
+
+        mock_osv.return_value = {
+            "vulns": [{"id": "GHA-xxxx-xxxx"}, {"id": "GHA-yyyy-yyyy"}]
+        }
+
+        filter_obj = _Utils.fetch("FakePkg", "f3.2")
+        entry = filter_obj.reports
+        expected = [{"id": "GHA-xxxx-xxxx"}, {"id": "GHA-yyyy-yyyy"}]
+
+        assert entry == expected
